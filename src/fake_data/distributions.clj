10bb3b23cd07d6b48973a9afb421c3f7d034b3b5
rmk2 <ryko@rmk2.org>
2020-07-16 23:43:24 +0200

    Remove unused requirements from distributions.clj

--- src/fake_data/distributions.clj
+++ src/fake_data/distributions.clj
@@ -1,7 +1,5 @@
 (ns fake-data.distributions
-  (:require [clojure.spec.alpha :as s]
-            [clojure.spec.gen.alpha :as gen]
-            [clojure.spec.test.alpha :as stest]))
+  (:require [clojure.spec.alpha :as s]))
 
 (defn triangular
   "Triangular distribution"
