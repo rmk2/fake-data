d5ed488844a55930f8e27cc4d772e53aba22736e
rmk2 <ryko@rmk2.org>
2020-07-16 23:43:56 +0200

    Add seasonality support to generate day, add keyword args
    This commit actually adds support for weights/seasonality to generate-days,
    which means the number of base timestamps per minute can be modified for
    individual hours, thus generating trends or skew in the generated time series.
    
    This commit also refactors the multi-arity options for generate-days into
    keyword support, which makes it easier to supply options independently of each
    other.

--- src/fake_data/seasonality.clj
+++ src/fake_data/seasonality.clj
@@ -1,25 +1,30 @@
 (ns fake-data.seasonality
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [fake-data.generation :as gen]))
 
 (def ^{:dynamic true} *default-offset* (java.time.ZoneOffset/ofHoursMinutes 0 0))
 (def ^{:dynamic true} *default-date* (java.time.LocalDate/now))
 
 (defn generate-day
   "Generate <base> timestamps per minute, for a whole day"
-  ([base] (generate-day base *default-date* *default-offset*))
-  ([base date] (generate-day base date *default-offset*))
-  ([base date offset]
-   {:pre [(s/valid? ::generate-day [base date offset])]
+  [base & {:keys [date offset weights]
+           :or {date *default-date* offset *default-offset* weights nil}}]
+   {:pre [(s/valid? ::generate-day [base date offset weights])]
     :post [(seq? %) (every? #(instance? java.time.OffsetDateTime %) %)]}
    (->
     (for [hour (range 0 24)]
       (for [minute (range 0 60)]
-        (for [x (range 0 base)]
-          (-> (java.time.OffsetTime/of hour minute (rand-int 60) 0 offset) (.atDate date)))))
-    (flatten))))
+        (let [weight (or (nth weights hour) 1.0)]
+          (for [x (range 0 (gen/add-weight base weight))]
+            (-> (java.time.OffsetTime/of hour minute (rand-int 60) 0 offset) (.atDate date))))))
+    (flatten)))
 
-(s/def ::generate-day (s/cat :base pos-int? :date #(instance? java.time.LocalDate %) :offset #(instance? java.time.ZoneOffset %)))
+(s/def ::generate-day (s/cat :base pos-int?
+                             :date #(instance? java.time.LocalDate %)
+                             :offset #(instance? java.time.ZoneOffset %)
+                             :weights (s/nilable (s/and (s/coll-of float) #(= (count %) 24)))))
 
 (s/fdef generate-day
   :args ::generate-day
   :ret (s/every #(instance? java.time.OffsetDateTime %)))
+
